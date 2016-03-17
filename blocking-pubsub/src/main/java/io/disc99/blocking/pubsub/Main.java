163d3f4a19a095c9c2fdc3cd5918d9806c5d886c
d.kanehira <disc99.g@gmail.com>
2016-03-17 19:14:17 +0900

    Add some classes

--- blocking-pubsub/src/main/java/io/disc99/blocking/pubsub/Main.java
+++ blocking-pubsub/src/main/java/io/disc99/blocking/pubsub/Main.java
@@ -1,30 +1,81 @@
 package io.disc99.blocking.pubsub;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+
 public class Main {
+
+    public static void main(String[] args) {
+        ReservationService reservationService = new ReservationService();
+        ReservationResponse response = reservationService.execute(new ReservationRequest());
+
+    }
+}
+
+class ReservationService {
+    ReservationResponse execute(ReservationRequest request) {
+
+
+        return null;
+    }
 }
 
 
 class DbService {
-
+    void update(Object data) {
+        Util.log("DbService#update: %s", data);
+        Util.sleep(1_000);
+    }
 }
 
 class NotificationService {
+    void send(Object data) {
+        Util.log("NotificationService#send: %s", data);
+        Util.sleep(2_000);
+    }
+}
+
+@Data @AllArgsConstructor @NoArgsConstructor
+class ReservationRequest {
 
 }
 
-class ReservationService {
+@Data @AllArgsConstructor @NoArgsConstructor
+class ReservationResponse {
 
 }
 
+@Data @AllArgsConstructor @NoArgsConstructor
 class RreservationExecuted {
-
+    String reservationId;
 }
 
+@Data @AllArgsConstructor @NoArgsConstructor
 class RreservationCompleted {
-
+    String reservationId;
 }
 
 
+@Data @AllArgsConstructor @NoArgsConstructor
 class RreservationNotified {
+    String reservationId;
+}
+
+
+
+
+
+// Sample support
+class Util {
+    @SneakyThrows
+    static void sleep(long time) {
+        Thread.sleep(time);
+    }
+
+    static void log(String format, Object... params) {
+        System.out.println(String.format(format, params));
+    }
 
 }
